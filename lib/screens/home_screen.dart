8330f186e30e08ac8101f61ac845c25f0f8a1f09
antheiz <salupacode@gmail.com>
2025-03-04 13:11:17 +0900

    fix: reduce spacing in home screen layout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -88,7 +88,7 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-                  const SizedBox(height: 16),
+                  const SizedBox(height: 10),
                   InkWell(
                     onTap: () {
                       Navigator.of(context).push(
