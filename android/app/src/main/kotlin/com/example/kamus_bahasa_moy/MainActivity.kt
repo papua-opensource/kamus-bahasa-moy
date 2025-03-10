6d30b532bfb387165217db1b6df51598dd6915a7
antheiz <salupacode@gmail.com>
2025-03-10 23:17:16 +0900

    refactor: update Java version to 11 and relocate MainActivity to new package

--- android/app/src/main/kotlin/com/example/kamus_bahasa_moy/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kamus_bahasa_moy
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/org/papuaopensource/kamus_bahasa_moy/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.papuaopensource.kamus_bahasa_moy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
